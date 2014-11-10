20379dfb8c6f35f88ad0070a3eef698ca88bd302
Vnu <dearvnu@yahoo.in>
2014-11-09 20:35:54 -0800

    Added comments and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 Tipster
 =======
 
-Simple TipCalculator App in Swift
++ Simple TipCalculator App in Swift. My First App in Swift. 
++ Some naive steps in detail for my own reference. 
+
+##### Initial Setup
+1. Create a new File -> Project -> Single Page Application
+2. Set your simulator, deployment Target if needed etc., in General View
+**Image 1**
+
+##### Create your layouts
+**Image2**
+1. Open Main.storyboard
+2. Start dragging your labels and text boxes, and align them and give some placeholder values 
+3. UISegementControl used for tip percentage
+4. UITextField for entering the value
+5. The black bar is just a UIView with black background
+6. Majority of the other fields are UILabels
+
+##### Creating the outlets
+1. Use the intersected circles on top right to open the editor
+2. In your view editor, click the element. Press ctrl and drag to your controller
+3. Add the name, format eg., tipLabel -> Add the type of element for easy identification
+4. After outlets, start creating your actions the same way ctrl + drag and change options accordingly
+**Image3**
+
+#### Resources used
++ http://vimeo.com/102084767
+
+
+
--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -4,17 +4,20 @@
 //
 //  Created by Vinu Charanya on 11/9/14.
 //  Copyright (c) 2014 Zunivo. All rights reserved.
-//
+//  Reference Video: http://vimeo.com/102084767
 
 import UIKit
 
 class ViewController: UIViewController {
 
+//  Initiating the view elements in the controller as Outlets
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    // What should happen on ViewLoad
+    // Here it sets the values to 0.00
     override func viewDidLoad() {
         super.viewDidLoad()
         tipLabel.text = "$0.00"
@@ -27,12 +30,14 @@ class ViewController: UIViewController {
     }
 
     // To Hide the keyboard on Tap
+    // Uses Tap gesture
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
     
     
-    // On Editing Bill Amount
+    // Action On Editing Bill Amount
+    // On Value changed for segmentControl
     @IBAction func onEditingChanged(sender: AnyObject) {
         var tipPercentages = [0.18, 0.2, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
@@ -51,7 +56,7 @@ class ViewController: UIViewController {
         
 //        tipLabel.text = "$\(tip)"
 //        totalLabel.text = "$\(total)"
-//        
+//        Using the following over above to help format the string output
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
